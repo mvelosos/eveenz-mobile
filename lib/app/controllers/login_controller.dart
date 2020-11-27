063d11ec9be22fbdbef5cce2dbd4129f551861fb
Mateus Veloso <mfvelosos@gmail.com>
2020-11-27 18:58:31 -0300

    impolemented login_controller

--- lib/app/controllers/login_controller.dart
+++ lib/app/controllers/login_controller.dart
@@ -1,1 +1,18 @@
-class LoginController {}
+import 'package:dartz/dartz.dart';
+import 'package:party_mobile/app/models/auth_user_model.dart';
+import 'package:party_mobile/app/repositories/auth_repository.dart';
+import 'package:party_mobile/app/shared/errors/errors.dart';
+import 'package:party_mobile/app/view_models/user_login_vm.dart';
+
+class LoginController {
+  AuthRepository _authRepository;
+
+  LoginController() {
+    _authRepository = AuthRepository();
+  }
+
+  Future<Either<Failure, AuthUserModel>> loginWithEmail(
+      UserLoginVM userLogin) async {
+    var authUser = await _authRepository.authLogin(userLogin);
+  }
+}
