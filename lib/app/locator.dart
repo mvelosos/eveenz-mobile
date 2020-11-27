cc11f4070a008566df44e7d6cfed9f63e8acc690
Mateus Veloso <mfvelosos@gmail.com>
2020-11-27 01:00:20 -0300

    update locator to include LoginController

--- lib/app/locator.dart
+++ lib/app/locator.dart
@@ -1,10 +1,15 @@
 import 'package:get_it/get_it.dart';
+import 'package:party_mobile/app/controllers/login_controller.dart';
 import 'package:party_mobile/app/services/navigation_service.dart';
 import 'package:party_mobile/app/shared/utils/dio_http.dart';
 
 GetIt locator = GetIt.instance;
 
 void setupLocator() {
+  // Utils
   locator.registerLazySingleton(() => NavigationService());
   locator.registerLazySingleton(() => DioHttp());
+
+  // Controllers
+  locator.registerLazySingleton(() => LoginController());
 }
