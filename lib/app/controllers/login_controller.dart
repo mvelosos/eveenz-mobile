008da640bb3a3b0cbf545bc590d3d6656e12dc7f
Mateus Veloso <mfvelosos@gmail.com>
2020-11-28 19:08:52 -0300

    update login_controller

--- lib/app/controllers/login_controller.dart
+++ lib/app/controllers/login_controller.dart
@@ -1,8 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:party_mobile/app/models/auth_user_model.dart';
 import 'package:party_mobile/app/repositories/auth_repository.dart';
+import 'package:party_mobile/app/services/local_storage_service.dart';
 import 'package:party_mobile/app/shared/errors/errors.dart';
 import 'package:party_mobile/app/view_models/user_login_vm.dart';
+
+import '../locator.dart';
 
 class LoginController {
   AuthRepository _authRepository;
@@ -14,5 +17,11 @@ class LoginController {
   Future<Either<Failure, AuthUserModel>> loginWithEmail(
       UserLoginVM userLogin) async {
     var authUser = await _authRepository.authLogin(userLogin);
+    authUser.fold((l) => null, (r) => {_setLocalStorage(r), print(123)});
+  }
+
+  _setLocalStorage(AuthUserModel authUser) {
+    var localStorage = locator<LocalStorageService>();
+    localStorage.put('party@jwt_token', authUser.token);
   }
 }
