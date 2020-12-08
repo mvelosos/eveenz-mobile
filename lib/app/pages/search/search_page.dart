20f8038a7e280adf63cdce7d6d6e9eee83dbea8f
Mateus Veloso <mfvelosos@gmail.com>
2020-12-08 20:18:12 -0300

    created search_page

---
+++ lib/app/pages/search/search_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Search',
+          style: TextStyle(color: Colors.blue),
+        ),
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+      ),
+      body: Center(child: Text('This is the search page!')),
+    );
+  }
+}
