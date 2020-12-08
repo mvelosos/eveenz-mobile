0bcaaf643f2810be92f015687ee1d729a455f97c
Mateus Veloso <mfvelosos@gmail.com>
2020-12-08 20:18:01 -0300

    add app_container route name

--- lib/app/shared/constants/route_names.dart
+++ lib/app/shared/constants/route_names.dart
@@ -1,6 +1,7 @@
 // This file contains all the routing constants used within the app
 class RouteNames {
   static const String root = '/';
+  static const String appContainer = '/app_container';
   static const String login = '/login';
   static const String home = '/home';
 }
