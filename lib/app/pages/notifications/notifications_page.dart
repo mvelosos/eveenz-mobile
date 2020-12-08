69abbdf857c5dc482963618f11b6867869730d9e
Mateus Veloso <mfvelosos@gmail.com>
2020-12-08 20:17:32 -0300

    created notifications_page

---
+++ lib/app/pages/notifications/notifications_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class NotificationsPage extends StatefulWidget {
+  @override
+  _NotificationsPageState createState() => _NotificationsPageState();
+}
+
+class _NotificationsPageState extends State<NotificationsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Notifications',
+          style: TextStyle(color: Colors.blue),
+        ),
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+      ),
+      body: Center(child: Text('This is the notifications page!')),
+    );
+  }
+}
