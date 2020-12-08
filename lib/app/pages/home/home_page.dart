9b94d4e4ba32aa7d67524500a9eee31cd3b955fc
Mateus Veloso <mfvelosos@gmail.com>
2020-12-08 20:17:02 -0300

    extract bottom_tab_navigation from home_page

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -8,22 +8,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _currentIndex = 0;
-
-  final List<Widget> _telas = [
-    Container(child: Text('Home'), color: Colors.white),
-    Container(child: Text('Search'), color: Colors.white),
-    Container(child: Text('Map'), color: Colors.white),
-    Container(child: Text('List'), color: Colors.white),
-    Container(child: Text('Profile'), color: Colors.white),
-  ];
-
-  void onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,21 +19,7 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.transparent,
         shadowColor: Colors.transparent,
       ),
-      body: _telas[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: onTabTapped,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
-          BottomNavigationBarItem(label: 'Search', icon: Icon(Icons.search)),
-          BottomNavigationBarItem(label: 'Map', icon: Icon(Icons.map)),
-          BottomNavigationBarItem(label: 'List', icon: Icon(Icons.view_list)),
-          BottomNavigationBarItem(label: 'U', icon: Icon(Icons.account_circle))
-        ],
-      ),
+      body: Center(child: Text('This is the home page!')),
     );
   }
 }
