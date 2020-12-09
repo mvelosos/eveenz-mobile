488a292616ed31ca6c4e7a887c057cfc2bf01c28
Mateus Veloso <mfvelosos@gmail.com>
2020-12-09 01:26:23 -0300

    added google maps to app

--- lib/app/pages/map/map_page.dart
+++ lib/app/pages/map/map_page.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapPage extends StatefulWidget {
   @override
@@ -6,18 +9,71 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  // TODO: Update this later
+  CameraPosition _initialPosition = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  // TODO: Update this later
+  @override
+  void initState() {
+    super.initState();
+    Future<Position> position = _determinePosition();
+    position.then((value) => {
+          setState(() {
+            _initialPosition = CameraPosition(
+              target: LatLng(value.latitude, value.longitude),
+              zoom: 14.4746,
+            );
+          })
+        });
+  }
+
+  Completer<GoogleMapController> _gmController = Completer();
+
+  // TODO: Update this later
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permantly denied, we cannot request permissions.');
+    }
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission != LocationPermission.whileInUse &&
+          permission != LocationPermission.always) {
+        return Future.error(
+            'Location permissions are denied (actual value: $permission).');
+      }
+    }
+
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.best);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Map',
-          style: TextStyle(color: Colors.blue),
-        ),
-        backgroundColor: Colors.transparent,
-        shadowColor: Colors.transparent,
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _initialPosition,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        rotateGesturesEnabled: false,
+        onMapCreated: (GoogleMapController controller) {
+          _gmController.complete(controller);
+        },
       ),
-      body: Center(child: Text('This is the map page!')),
     );
   }
 }
