02480703fc849dc8e70633389a36160d82583dd4
Mateus Veloso <mfvelosos@gmail.com>
2020-12-11 12:22:19 -0300

    updated profile just for now :D

--- lib/app/pages/profile/profile_page.dart
+++ lib/app/pages/profile/profile_page.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:party_mobile/app/locator.dart';
+import 'package:party_mobile/app/services/local_storage_service.dart';
+import 'package:party_mobile/app/services/navigation_service.dart';
+import 'package:party_mobile/app/shared/constants/route_names.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
   _ProfilePageState createState() => _ProfilePageState();
 }
 
+// TODO: Reafactor this class later
 class _ProfilePageState extends State<ProfilePage> {
+  var storage = locator<LocalStorageService>();
+  var navigation = locator<NavigationService>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +25,17 @@ class _ProfilePageState extends State<ProfilePage> {
         backgroundColor: Colors.transparent,
         shadowColor: Colors.transparent,
       ),
-      body: Center(child: Text('This is the user profile page!')),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Logout'),
+          onPressed: () => {
+            storage.clear().then(
+                  (_) => navigation
+                      .pushNamedReplacementNoAnimation(RouteNames.login),
+                )
+          },
+        ),
+      ),
     );
   }
 }
