ae6329481a2c750a85cf644639cb824fc3795af2
Mateus Veloso <mfvelosos@gmail.com>
2020-12-11 17:44:30 -0300

    created forgot_password_page

---
+++ lib/app/pages/forgot_password/forgot_password_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ForgotPasswordPage extends StatefulWidget {
+  @override
+  _ForgotPasswordPageState createState() => _ForgotPasswordPageState();
+}
+
+class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Forgot Password',
+          style: TextStyle(color: Colors.blue),
+        ),
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+        iconTheme: IconThemeData(color: Colors.blue),
+      ),
+      body: Center(
+        child: Text('Forgot password page'),
+      ),
+    );
+  }
+}
