d5cfd2d69a55bc40668b75f6b5b5dc6acf7f9207
Mateus Veloso <mfvelosos@gmail.com>
2020-12-11 12:22:55 -0300

    updated IAuthRepositoru

--- lib/app/interfaces/auth_repository_interface.dart
+++ lib/app/interfaces/auth_repository_interface.dart
@@ -1,10 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:party_mobile/app/models/auth_user_model.dart';
 import 'package:party_mobile/app/shared/errors/errors.dart';
+import 'package:party_mobile/app/view_models/facebook_login_vm.dart';
 import 'package:party_mobile/app/view_models/user_login_vm.dart';
 
 abstract class IAuthRepositoryInterface {
   Future<Either<Failure, AuthUserModel>> authLogin(UserLoginVM userLogin);
 
-  Future<Either<Failure, AuthUserModel>> authFacebook({String accessToken});
+  Future<Either<Failure, AuthUserModel>> authFacebook(
+      {FacebookLoginVM fbLogin});
 }
