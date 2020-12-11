03e161256c63ac0d39bd27e6c7a7381ae4413470
Mateus Veloso <mfvelosos@gmail.com>
2020-12-11 17:24:59 -0300

    created sign_up page

---
+++ lib/app/pages/signup/signup_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'SignUp',
+          style: TextStyle(color: Colors.blue),
+        ),
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+        iconTheme: IconThemeData(color: Colors.blue),
+      ),
+      body: Center(
+        child: Text('This is the signup page!'),
+      ),
+    );
+  }
+}
