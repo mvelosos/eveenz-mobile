bd35392713a06c5625095c502a1beee1acb08b87
Mateus Veloso <mfvelosos@gmail.com>
2020-12-15 02:06:24 -0300

    load profile on app_container

--- lib/app/pages/app_container/app_container.dart
+++ lib/app/pages/app_container/app_container.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:party_mobile/app/controllers/profile_controller.dart';
+import 'package:party_mobile/app/locator.dart';
 import 'package:party_mobile/app/pages/app_container/widgets/bottom_navigation_bar_widget.dart';
 import 'package:party_mobile/app/pages/home/home_page.dart';
 import 'package:party_mobile/app/pages/map/map_page.dart';
@@ -12,9 +14,16 @@ class AppContainer extends StatefulWidget {
 }
 
 class _AppContainerState extends State<AppContainer> {
+  var _profileController = locator<ProfileController>();
   int _currentIndex = 0;
 
   // Functions
+
+  @override
+  void initState() {
+    super.initState();
+    _profileController.getProfile();
+  }
 
   void _onTabTapped(int index) {
     setState(() {
