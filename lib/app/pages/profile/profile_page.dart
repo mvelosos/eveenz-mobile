bdb754f87de04fa8684bf5615362fdd31730c564
Mateus Veloso <mfvelosos@gmail.com>
2020-12-15 02:07:02 -0300

    updated profile_page

--- lib/app/pages/profile/profile_page.dart
+++ lib/app/pages/profile/profile_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:party_mobile/app/locator.dart';
 import 'package:party_mobile/app/services/local_storage_service.dart';
 import 'package:party_mobile/app/services/navigation_service.dart';
 import 'package:party_mobile/app/shared/constants/route_names.dart';
+import 'package:party_mobile/app/stores/profile_store.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -10,8 +12,10 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  var storage = locator<LocalStorageService>();
-  var navigation = locator<NavigationService>();
+  // TODO: Remove this from here \/
+  var _storage = locator<LocalStorageService>();
+  var _navigation = locator<NavigationService>();
+  var _profileStore = locator<ProfileStore>();
 
   @override
   Widget build(BuildContext context) {
@@ -25,15 +29,20 @@ class _ProfilePageState extends State<ProfilePage> {
         shadowColor: Colors.transparent,
       ),
       body: Center(
-        child: RaisedButton(
-          child: Text('Logout'),
-          onPressed: () => {
-            storage.clear().then(
-                  (_) => navigation
-                      .pushNamedReplacementNoAnimation(RouteNames.login),
-                )
-          },
-        ),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Observer(
+            builder: (_) => Text(_profileStore.username),
+          ),
+          RaisedButton(
+            child: Text('Logout'),
+            onPressed: () => {
+              _storage.clear().then(
+                    (_) => _navigation
+                        .pushNamedReplacementNoAnimation(RouteNames.login),
+                  )
+            },
+          ),
+        ]),
       ),
     );
   }
