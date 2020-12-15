4695e2ce2584dd69bbe34c185d37aa34601d95f6
Mateus Veloso <mfvelosos@gmail.com>
2020-12-14 23:14:57 -0300

    user login_store on login_controller

--- lib/app/controllers/login_controller.dart
+++ lib/app/controllers/login_controller.dart
@@ -8,33 +8,38 @@ import 'package:party_mobile/app/services/navigation_service.dart';
 import 'package:party_mobile/app/shared/constants/route_names.dart';
 import 'package:party_mobile/app/shared/constants/storage.dart';
 import 'package:party_mobile/app/shared/errors/errors.dart';
+import 'package:party_mobile/app/stores/login_store.dart';
 import 'package:party_mobile/app/view_models/facebook_login_vm.dart';
 import 'package:party_mobile/app/view_models/user_login_vm.dart';
 
 import '../locator.dart';
 
-// TODO: Refactor this class later
 class LoginController {
   AuthRepository _authRepository;
   NavigationService _navigation;
+  LoginStore _loginStore;
 
   LoginController() {
     _authRepository = AuthRepository();
     _navigation = locator<NavigationService>();
+    _loginStore = locator<LoginStore>();
   }
 
   Future<Either<Failure, AuthUserModel>> loginWithEmail(
       UserLoginVM userLogin) async {
+    _loginStore.setLoading(true);
     var authResult = await _authRepository.authLogin(userLogin);
     if (authResult.isRight()) {
       _setLocalStorage(authResult.getOrElse(null));
       _navigation.pushNamed(RouteNames.appContainer);
     }
+    _loginStore.setLoading(false);
     return authResult;
   }
 
   Future<Either<Failure, AuthUserModel>> loginWithFacebook(
       FacebookLoginVM fbLogin) async {
+    _loginStore.setLoading(true);
     var authUser = await _authRepository.authFacebook(fbLogin);
     authUser.fold(
         (l) => null,
@@ -42,6 +47,7 @@ class LoginController {
               _setLocalStorage(r),
               _navigation.pushNamed(RouteNames.appContainer)
             });
+    _loginStore.setLoading(false);
   }
 
   _setLocalStorage(AuthUserModel authUser) {
