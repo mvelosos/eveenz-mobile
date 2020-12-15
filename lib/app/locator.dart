fe81a242084a10ef6e33c163026eeaeb1a18546b
Mateus Veloso <mfvelosos@gmail.com>
2020-12-15 01:22:31 -0300

    updated locator

--- lib/app/locator.dart
+++ lib/app/locator.dart
@@ -1,10 +1,12 @@
 import 'package:get_it/get_it.dart';
 import 'package:party_mobile/app/controllers/login_controller.dart';
+import 'package:party_mobile/app/controllers/profile_controller.dart';
 import 'package:party_mobile/app/services/local_storage_service.dart';
 import 'package:party_mobile/app/services/navigation_service.dart';
 import 'package:party_mobile/app/shared/utils/dio_http.dart';
 import 'package:party_mobile/app/stores/auth_user_store.dart';
 import 'package:party_mobile/app/stores/login_store.dart';
+import 'package:party_mobile/app/stores/profile_store.dart';
 
 GetIt locator = GetIt.instance;
 
@@ -16,8 +18,10 @@ void setupLocator() {
 
   // Controllers
   locator.registerLazySingleton(() => LoginController());
+  locator.registerLazySingleton(() => ProfileController());
 
   //Stores
   locator.registerLazySingleton(() => LoginStore());
   locator.registerLazySingleton(() => AuthUserStore());
+  locator.registerLazySingleton(() => ProfileStore());
 }
