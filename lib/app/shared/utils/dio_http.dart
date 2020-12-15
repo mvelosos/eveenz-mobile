33211b5ec8a64c5768448dedd1ea8c6b316e9bd1
Mateus Veloso <mfvelosos@gmail.com>
2020-12-15 02:06:37 -0300

    updated dio to add interceptor

--- lib/app/shared/utils/dio_http.dart
+++ lib/app/shared/utils/dio_http.dart
@@ -1,31 +1,44 @@
 import 'package:dio/dio.dart';
+import 'package:party_mobile/app/locator.dart';
+import 'package:party_mobile/app/services/local_storage_service.dart';
+import 'package:party_mobile/app/shared/constants/storage.dart';
 import 'package:party_mobile/app/shared/constants/urls.dart';
 
 class DioHttp {
   Dio _dio;
+  LocalStorageService _localStorageService;
 
   DioHttp() {
     _dio = Dio();
     _dio.options.baseUrl = Urls().apiUrl;
-    _dio.interceptors.add(InterceptorsWrapper(onError: _onError));
+    _dio.interceptors
+        .add(InterceptorsWrapper(onError: _onError, onResponse: _onResponse));
+    _localStorageService = locator<LocalStorageService>();
   }
 
-  Dio get instance => _dio;
-
   Dio withAuth() {
-    _dio.interceptors.add(InterceptorsWrapper(
-        onRequest: _onRequest, onResponse: _onRespose, onError: _onError));
+    _dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: _onRequest,
+        onResponse: _onResponse,
+        onError: _onError,
+      ),
+    );
 
     return _dio;
   }
 
+  Dio get instance => _dio;
+
   _onRequest(RequestOptions options) async {
-    // SharedPreferences prefs = await SharedPreferences.getInstance();
-    // var token = prefs.get('token');
-    // options.headers['Authorization'] = token;
+    _localStorageService.get(Storage.jwtToken).then(
+          (value) => {
+            if (value != null) {options.headers['Authorization'] = value}
+          },
+        );
   }
 
-  _onRespose(Response e) {
+  _onResponse(Response e) {
     print(e.data);
   }
 
