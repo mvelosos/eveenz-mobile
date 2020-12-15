dff0cfbd44b45bdc610bd9a1de3f7192061d85c0
Mateus Veloso <mfvelosos@gmail.com>
2020-12-15 01:04:24 -0300

    created endpoint for me

--- lib/app/shared/constants/endpoints.dart
+++ lib/app/shared/constants/endpoints.dart
@@ -2,4 +2,7 @@ class Endpoints {
   // Auth Endpoints
   static const String authLogin = '/auth/login';
   static const String authFacebook = '/auth/facebook';
+
+  // Profile Endpoints
+  static const String me = '/me';
 }
