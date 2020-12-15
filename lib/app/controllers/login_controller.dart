3c470467e42ee2c5bbafc227e9acbf4830ef0f48
Mateus Veloso <mfvelosos@gmail.com>
2020-12-14 23:38:26 -0300

    updated login_controller to set auth_user_store

--- lib/app/controllers/login_controller.dart
+++ lib/app/controllers/login_controller.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:dartz/dartz.dart';
+import 'package:party_mobile/app/locator.dart';
 import 'package:party_mobile/app/models/auth_user_model.dart';
 import 'package:party_mobile/app/repositories/auth_repository.dart';
 import 'package:party_mobile/app/services/local_storage_service.dart';
@@ -8,21 +9,22 @@ import 'package:party_mobile/app/services/navigation_service.dart';
 import 'package:party_mobile/app/shared/constants/route_names.dart';
 import 'package:party_mobile/app/shared/constants/storage.dart';
 import 'package:party_mobile/app/shared/errors/errors.dart';
+import 'package:party_mobile/app/stores/auth_user_store.dart';
 import 'package:party_mobile/app/stores/login_store.dart';
 import 'package:party_mobile/app/view_models/facebook_login_vm.dart';
 import 'package:party_mobile/app/view_models/user_login_vm.dart';
-
-import '../locator.dart';
 
 class LoginController {
   AuthRepository _authRepository;
   NavigationService _navigation;
   LoginStore _loginStore;
+  AuthUserStore _authUserStore;
 
   LoginController() {
     _authRepository = AuthRepository();
     _navigation = locator<NavigationService>();
     _loginStore = locator<LoginStore>();
+    _authUserStore = locator<AuthUserStore>();
   }
 
   Future<Either<Failure, AuthUserModel>> loginWithEmail(
@@ -31,6 +33,7 @@ class LoginController {
     var authResult = await _authRepository.authLogin(userLogin);
     if (authResult.isRight()) {
       _setLocalStorage(authResult.getOrElse(null));
+      _authUserStore.setUser(authResult.getOrElse(null));
       _navigation.pushNamed(RouteNames.appContainer);
     }
     _loginStore.setLoading(false);
@@ -40,14 +43,14 @@ class LoginController {
   Future<Either<Failure, AuthUserModel>> loginWithFacebook(
       FacebookLoginVM fbLogin) async {
     _loginStore.setLoading(true);
-    var authUser = await _authRepository.authFacebook(fbLogin);
-    authUser.fold(
-        (l) => null,
-        (r) => {
-              _setLocalStorage(r),
-              _navigation.pushNamed(RouteNames.appContainer)
-            });
+    var authResult = await _authRepository.authFacebook(fbLogin);
+    if (authResult.isRight()) {
+      _setLocalStorage(authResult.getOrElse(null));
+      _authUserStore.setUser(authResult.getOrElse(null));
+      _navigation.pushNamed(RouteNames.appContainer);
+    }
     _loginStore.setLoading(false);
+    return authResult;
   }
 
   _setLocalStorage(AuthUserModel authUser) {
