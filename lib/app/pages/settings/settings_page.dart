9cb9fba1597311eafe75b7155c3a967852e9b405
Mateus Veloso <mfvelosos@gmail.com>
2020-12-16 00:45:02 -0300

    created settings page

---
+++ lib/app/pages/settings/settings_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:party_mobile/app/locator.dart';
+import 'package:party_mobile/app/services/local_storage_service.dart';
+import 'package:party_mobile/app/services/navigation_service.dart';
+import 'package:party_mobile/app/shared/constants/route_names.dart';
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  var _storage = locator<LocalStorageService>();
+  var _navigation = locator<NavigationService>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+        brightness: Brightness.light,
+        iconTheme: IconThemeData(color: Colors.blue),
+      ),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          RaisedButton(
+            child: Text('Logout'),
+            onPressed: () => {
+              _storage.clear().then(
+                    (_) => _navigation
+                        .pushNamedReplacementNoAnimation(RouteNames.login),
+                  )
+            },
+          ),
+        ]),
+      ),
+    );
+  }
+}
