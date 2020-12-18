453bc867b684fffc373f501f1f0a2111df518d02
Mateus Veloso <mfvelosos@gmail.com>
2020-12-18 00:20:10 -0300

    update urls

--- lib/app/shared/constants/urls.dart
+++ lib/app/shared/constants/urls.dart
@@ -1,7 +1,8 @@
 class Urls {
   static const String protocol = 'http';
   static const String apiVersion = 'v1';
-  static const String baseUrl = '10.0.2.2:3000';
+  // static const String baseUrl = '10.0.2.2:3000'; //Use this when running on Android Emulator
+  static const String baseUrl = '127.0.0.1:3000';
 
   String get apiUrl {
     return '$protocol://$baseUrl/api/$apiVersion';
