03989c2d9280a6e66e16d91aa29010df4757b6b2
Mateus Veloso <mfvelosos@gmail.com>
2020-12-23 20:01:39 -0300

    updated search_page

--- lib/app/pages/search/search_page.dart
+++ lib/app/pages/search/search_page.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -9,17 +12,63 @@ class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Search',
-          style: TextStyle(color: Colors.blue),
-        ),
-        backgroundColor: Colors.transparent,
-        shadowColor: Colors.transparent,
-        brightness: Brightness.light,
-        automaticallyImplyLeading: false,
-      ),
-      body: Center(child: Text('This is the search page!')),
+      body: LayoutBuilder(builder: (context, constraints) {
+        return SafeArea(
+          child: GestureDetector(
+            onTap: () {
+              FocusScope.of(context).unfocus();
+            },
+            child: Container(
+              color: Colors.transparent,
+              padding: EdgeInsets.only(left: 10, right: 10),
+              height: constraints.maxHeight,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Buscar',
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.blue,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 10),
+                      child: TextField(
+                        onChanged: (value) {
+                          print(value);
+                        },
+                        autocorrect: false,
+                        enableSuggestions: false,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.search),
+                          labelText: 'Buscar por eventos ou pessoas',
+                          floatingLabelBehavior: FloatingLabelBehavior.never,
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20),
+                            ),
+                            borderSide: BorderSide(
+                              color: Colors.blue,
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20),
+                            ),
+                            borderSide: BorderSide(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ]),
+            ),
+          ),
+        );
+      }),
     );
   }
 }
