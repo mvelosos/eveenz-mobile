86d68b938a02272180a9d18ec4ea58ae3e84d512
Mateus Veloso <mfvelosos@gmail.com>
2020-12-24 17:34:36 -0300

    create custom search_list_view

---
+++ lib/app/pages/search/widgets/search_list_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:party_mobile/app/models/search_result_model.dart';
+import 'package:party_mobile/app/pages/search/widgets/account_list_tile.dart';
+import 'package:party_mobile/app/pages/search/widgets/event_list_tile.dart';
+
+class SearchListView extends StatelessWidget {
+  final SearchResultModel resultSearch;
+
+  SearchListView(this.resultSearch);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: resultSearch.data.length,
+      itemBuilder: (_, idx) {
+        if (resultSearch.data != null && resultSearch.data.length > 0) {
+          return resultSearch.data[idx].type == 'account'
+              ? AccountListTile(resultSearch.data[idx])
+              : EventListTile(resultSearch.data[idx]);
+        }
+        return null;
+      },
+    );
+  }
+}
