940e9d35e77f14707104d89fb8998e80cf9b8d99
Mateus Veloso <mfvelosos@gmail.com>
2020-12-24 17:34:49 -0300

    created tile_image

---
+++ lib/app/pages/search/widgets/tile_image.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class TileImage extends StatelessWidget {
+  final _item;
+
+  TileImage(this._item);
+
+  @override
+  Widget build(BuildContext context) {
+    var _url = _item.type == 'account' ? _item.avatarUrl : _item.imageUrl;
+
+    return _url != null
+        ? ClipRRect(
+            borderRadius: BorderRadius.circular(50),
+            child: Image.network(
+              _url,
+              height: 50,
+              width: 50,
+            ),
+          )
+        : CircleAvatar(
+            radius: 25,
+            backgroundColor: Colors.blue,
+          );
+  }
+}
