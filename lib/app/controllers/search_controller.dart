fbe28433cfa518801d9cdc06f8965c45cee9a20b
Mateus Veloso <mfvelosos@gmail.com>
2020-12-23 23:54:42 -0300

    created search_controller

---
+++ lib/app/controllers/search_controller.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+import 'package:dartz/dartz.dart';
+import 'package:party_mobile/app/models/search_result_model.dart';
+import 'package:party_mobile/app/repositories/search_repository.dart';
+import 'package:party_mobile/app/shared/errors/errors.dart';
+import 'package:party_mobile/app/view_models/search_vm.dart';
+
+class SearchController {
+  SearchRepository _searchRepository;
+
+  SearchController() {
+    _searchRepository = SearchRepository();
+  }
+
+  Future<Either<Failure, SearchResultModel>> search(SearchVM search) async {
+    var searchResult = await _searchRepository.search(search);
+    return searchResult;
+  }
+}
