562983f834fc7b40dd83312eba666f46e7ac9757
Mateus Veloso <mfvelosos@gmail.com>
2020-12-23 23:54:24 -0300

    created search endpoint

--- lib/app/shared/constants/endpoints.dart
+++ lib/app/shared/constants/endpoints.dart
@@ -3,6 +3,9 @@ class Endpoints {
   static const String authLogin = '/auth/login';
   static const String authFacebook = '/auth/facebook';
 
+  // Search Endpoints
+  static const String search = '/search';
+
   // Profile Endpoints
   static const String me = '/me';
 }
