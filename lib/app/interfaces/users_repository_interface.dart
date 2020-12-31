7369898f82c5aeded1e3598975c872fa3b77fb9b
Mateus Veloso <mfvelosos@gmail.com>
2020-12-31 19:14:12 -0300

    created users_repository

---
+++ lib/app/interfaces/users_repository_interface.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:party_mobile/app/models/auth_user_model.dart';
+import 'package:party_mobile/app/shared/errors/errors.dart';
+import 'package:party_mobile/app/view_models/create_user_vm.dart';
+
+abstract class IUsersRepository {
+  Future<Either<Failure, AuthUserModel>> createUser(CreateUserVM createUser);
+}
---
+++ lib/app/repositories/users_repository.dart
@@ -0,0 +1,32 @@
+import 'package:dartz/dartz.dart';
+import 'package:party_mobile/app/interfaces/users_repository_interface.dart';
+import 'package:party_mobile/app/locator.dart';
+import 'package:party_mobile/app/shared/constants/endpoints.dart';
+import 'package:party_mobile/app/shared/utils/dio_http.dart';
+import 'package:party_mobile/app/view_models/create_user_vm.dart';
+import 'package:party_mobile/app/shared/errors/errors.dart';
+import 'package:party_mobile/app/models/auth_user_model.dart';
+
+class UsersRepository implements IUsersRepository {
+  DioHttp _dio;
+
+  UsersRepository() {
+    _dio = locator<DioHttp>();
+  }
+
+  @override
+  Future<Either<Failure, AuthUserModel>> createUser(
+      CreateUserVM createUser) async {
+    try {
+      var user =
+          await _dio.instance.post(Endpoints.users, data: createUser.getData());
+      return Right(AuthUserModel.fromJson(user.data));
+    } catch (e) {
+      if (e.response.data['errors'].length > 0) {
+        var errors = e.response.data['errors'].join(', ');
+        return Left(CreateUserError(message: errors));
+      }
+      return Left(CreateUserError(message: 'Unexpected error!'));
+    }
+  }
+}
