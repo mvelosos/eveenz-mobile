8501268d518c58242b8c9ca96620590fe7138de7
Mateus Veloso <mfvelosos@gmail.com>
2021-01-07 01:33:39 -0300

    developed forgot_password_1_page

--- lib/app/pages/forgot_password/forgot_password_1_page.dart
+++ lib/app/pages/forgot_password/forgot_password_1_page.dart
@@ -8,19 +8,58 @@ class ForgotPassword1Page extends StatefulWidget {
 class _ForgotPassword1PageState extends State<ForgotPassword1Page> {
   @override
   Widget build(BuildContext context) {
+    final _size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Forgot Password 1',
-          style: TextStyle(color: Colors.blue),
-        ),
         backgroundColor: Colors.transparent,
         shadowColor: Colors.transparent,
         iconTheme: IconThemeData(color: Colors.blue),
         brightness: Brightness.light,
       ),
-      body: Center(
-        child: Text('Forgot password 1'),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return Stack(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  FocusScope.of(context).unfocus();
+                },
+                child: Container(
+                  height: constraints.maxHeight,
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                  color: Colors.transparent,
+                  child: Column(
+                    children: [
+                      Text(
+                        'Esqueceu sua senha?',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: _size.height * .03,
+                      ),
+                      TextField(
+                        autocorrect: false,
+                        enableSuggestions: false,
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.email),
+                          labelText: 'E-mail',
+                        ),
+                      ),
+                      SizedBox(
+                        height: _size.height * .03,
+                      ),
+                      RaisedButton(
+                        onPressed: () {},
+                        child: Text('Esqueci minha senha'),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
