2ce052bdc44165bbe9dced79bd5703f7aa4c1461
Mateus Veloso <mfvelosos@gmail.com>
2021-01-11 00:49:56 -0300

    organize file and add passwords/forgot endpoint

--- lib/app/shared/constants/endpoints.dart
+++ lib/app/shared/constants/endpoints.dart
@@ -1,19 +1,22 @@
 class Endpoints {
-  // Auth Endpoints
-  static const String authLogin = '/auth/login';
-  static const String authFacebook = '/auth/facebook';
-
-  // Search Endpoints
-  static const String search = '/search';
-
-  // Profile Endpoints
-  static const String me = '/me';
-
   // Accounts Endpoints
   static const String accountShow = '/accounts/:username';
   static const String accountFollowers = '/accounts/:username/followers';
   static const String accountFollowing = '/accounts/:username/following';
   static const String accountFollow = '/me/follows/accounts';
+
+  // Auth Endpoints
+  static const String authLogin = '/auth/login';
+  static const String authFacebook = '/auth/facebook';
+
+  // Passwords Endpoints
+  static const String passwordsForgot = '/passwords/forgot';
+
+  // Profile Endpoints
+  static const String me = '/me';
+
+  // Search Endpoints
+  static const String search = '/search';
 
   // Users Endpoints
   static const String users = '/users';
