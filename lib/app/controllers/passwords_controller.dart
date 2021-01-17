e2ecf8ebe7b7b0f1e9f46541f584df59a7d4fdfb
Mateus Veloso <mfvelosos@gmail.com>
2021-01-17 14:59:23 -0300

    implemented passwords_controller

--- lib/app/controllers/passwords_controller.dart
+++ lib/app/controllers/passwords_controller.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:party_mobile/app/repositories/passwords_repository.dart';
 import 'package:party_mobile/app/shared/errors/errors.dart';
 import 'package:party_mobile/app/view_models/password_forgot_email_vm.dart';
+import 'package:party_mobile/app/view_models/password_recovery_vm.dart';
 
 class PasswordsController {
   PasswordsRepository _passwordsRepository;
@@ -13,6 +14,18 @@ class PasswordsController {
   Future<Either<Failure, dynamic>> forgotPassword(
       PasswordForgotEmailVM passwordForgotEmail) async {
     var result = await _passwordsRepository.forgotPassword(passwordForgotEmail);
+    return result;
+  }
+
+  Future<Either<Failure, dynamic>> verifyCode(
+      PasswordRecoveryVM passwordRecovery) async {
+    var result = await _passwordsRepository.verifyCode(passwordRecovery);
+    return result;
+  }
+
+  Future<Either<Failure, dynamic>> recoverPassword(
+      PasswordRecoveryVM passwordRecovery) async {
+    var result = await _passwordsRepository.recoverPassword(passwordRecovery);
     return result;
   }
 }
