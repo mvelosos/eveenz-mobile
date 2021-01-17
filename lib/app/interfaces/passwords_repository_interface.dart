ede9fb51871656a046c77837e06a54e991852198
Mateus Veloso <mfvelosos@gmail.com>
2021-01-17 14:59:14 -0300

    implements passwords_repository

--- lib/app/interfaces/passwords_repository_interface.dart
+++ lib/app/interfaces/passwords_repository_interface.dart
@@ -1,8 +1,14 @@
 import 'package:dartz/dartz.dart';
 import 'package:party_mobile/app/shared/errors/errors.dart';
 import 'package:party_mobile/app/view_models/password_forgot_email_vm.dart';
+import 'package:party_mobile/app/view_models/password_recovery_vm.dart';
 
 abstract class IPasswordsRepository {
   Future<Either<Failure, Map>> forgotPassword(
       PasswordForgotEmailVM passwordForgotEmail);
+
+  Future<Either<Failure, Map>> verifyCode(PasswordRecoveryVM passwordRecovery);
+
+  Future<Either<Failure, Map>> recoverPassword(
+      PasswordRecoveryVM passwordRecovery);
 }
--- lib/app/repositories/passwords_repository.dart
+++ lib/app/repositories/passwords_repository.dart
@@ -5,6 +5,7 @@ import 'package:party_mobile/app/shared/constants/endpoints.dart';
 import 'package:party_mobile/app/shared/errors/errors.dart';
 import 'package:party_mobile/app/shared/utils/dio_http.dart';
 import 'package:party_mobile/app/view_models/password_forgot_email_vm.dart';
+import 'package:party_mobile/app/view_models/password_recovery_vm.dart';
 
 class PasswordsRepository implements IPasswordsRepository {
   DioHttp _dio;
@@ -19,6 +20,30 @@ class PasswordsRepository implements IPasswordsRepository {
     try {
       var result = await _dio.instance
           .post(Endpoints.passwordsForgot, data: passwordForgotEmail.getData());
+      return Right(result.data);
+    } catch (e) {
+      return Left(RequestError(message: e.response.data['error']));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Map>> verifyCode(
+      PasswordRecoveryVM passwordRecovery) async {
+    try {
+      var result = await _dio.instance.post(Endpoints.passwordsVerifyCode,
+          data: passwordRecovery.getData());
+      return Right(result.data);
+    } catch (e) {
+      return Left(RequestError(message: e.response.data['error']));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Map>> recoverPassword(
+      PasswordRecoveryVM passwordRecovery) async {
+    try {
+      var result = await _dio.instance
+          .post(Endpoints.passwordsRecover, data: passwordRecovery.getData());
       return Right(result.data);
     } catch (e) {
       return Left(RequestError(message: e.response.data['error']));
