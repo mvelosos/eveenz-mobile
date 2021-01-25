ec7e48fc22f0fce7c5d19769048381a6cde996df
Mateus Veloso <mfvelosos@gmail.com>
2021-01-24 22:27:31 -0300

    removed signup_store from signup_controller

--- lib/app/controllers/signup_controller.dart
+++ lib/app/controllers/signup_controller.dart
@@ -6,29 +6,24 @@ import 'package:party_mobile/app/repositories/users_repository.dart';
 import 'package:party_mobile/app/shared/errors/errors.dart';
 import 'package:party_mobile/app/shared/utils/commons.dart';
 import 'package:party_mobile/app/stores/auth_user_store.dart';
-import 'package:party_mobile/app/stores/signup_store.dart';
 import 'package:party_mobile/app/view_models/create_user_vm.dart';
 
 class SignUpController {
   UsersRepository _usersRepository;
-  SignUpStore _signUpStore;
   AuthUserStore _authUserStore;
 
   SignUpController() {
     _usersRepository = UsersRepository();
-    _signUpStore = locator<SignUpStore>();
     _authUserStore = locator<AuthUserStore>();
   }
 
   Future<Either<Failure, AuthUserModel>> createUser(
       CreateUserVM createUser) async {
-    _signUpStore.setLoading(true);
     var userResult = await _usersRepository.createUser(createUser);
     if (userResult.isRight()) {
       Commons.setAuthLocalStorage(userResult.getOrElse(null));
       _authUserStore.setUser(userResult.getOrElse(null));
     }
-    _signUpStore.setLoading(false);
     return userResult;
   }
 }
