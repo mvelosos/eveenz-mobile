e723d5956ac1d69187ef03d368afeb4bd06583f1
Mateus Veloso <mfvelosos@gmail.com>
2021-01-24 22:28:43 -0300

    implemented some funcions on commons

--- lib/app/shared/utils/commons.dart
+++ lib/app/shared/utils/commons.dart
@@ -4,14 +4,19 @@ import 'package:party_mobile/app/services/local_storage_service.dart';
 import 'package:party_mobile/app/shared/constants/storage.dart';
 
 class Commons {
-  static setAuthLocalStorage(AuthUserModel authUser) {
-    var localStorage = locator<LocalStorageService>();
-    localStorage.put(Storage.jwtToken, authUser.token);
-  }
-
   static bool matchEmailRegex(email) {
     return RegExp(
             r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
         .hasMatch(email);
+  }
+
+  static bool matchUsernameRegex(username) {
+    return RegExp(r"^[a-zA-Z0-9_.]+$").hasMatch(username);
+  }
+
+  static setAuthLocalStorage(AuthUserModel authUser) {
+    var localStorage = locator<LocalStorageService>();
+    localStorage.put(Storage.jwtToken, authUser.token);
+    localStorage.put(Storage.username, authUser.username);
   }
 }
