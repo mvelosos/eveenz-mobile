931ac4dbf09276e90a446ee652d8591b7b4d4199
Mateus Veloso <mfvelosos@gmail.com>
2021-02-06 21:16:43 -0300

    created googe atuth repo mehotd

--- lib/app/repositories/auth_repository.dart
+++ lib/app/repositories/auth_repository.dart
@@ -6,6 +6,7 @@ import 'package:party_mobile/app/shared/errors/errors.dart';
 import 'package:party_mobile/app/models/auth_user_model.dart';
 import 'package:party_mobile/app/shared/utils/dio_http.dart';
 import 'package:party_mobile/app/view_models/facebook_login_vm.dart';
+import 'package:party_mobile/app/view_models/google_login_vm.dart';
 import 'package:party_mobile/app/view_models/user_login_vm.dart';
 
 class AuthRepository implements IAuthRepositoryInterface {
@@ -33,6 +34,19 @@ class AuthRepository implements IAuthRepositoryInterface {
     try {
       var user = await _dio.instance
           .post(Endpoints.authFacebook, data: fbLogin.getData());
+
+      return Right(AuthUserModel.fromJson(user.data));
+    } catch (e) {
+      return Left(LoginError(message: e.response.data['errors']));
+    }
+  }
+
+  @override
+  Future<Either<Failure, AuthUserModel>> authGoogle(
+      GoogleLoginVM googleLogin) async {
+    try {
+      var user = await _dio.instance
+          .post(Endpoints.authGoogle, data: googleLogin.getData());
 
       return Right(AuthUserModel.fromJson(user.data));
     } catch (e) {
