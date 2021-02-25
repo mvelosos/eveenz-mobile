0bbdff0500d6e2d1e18f03695e6cd4d815b1cbbf
Mateus Veloso <mfvelosos@gmail.com>
2021-02-25 03:15:20 -0300

    lots of updates on new_event_3_page

--- lib/app/pages/new_event/new_event_3_page.dart
+++ lib/app/pages/new_event/new_event_3_page.dart
@@ -1,9 +1,12 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:party_mobile/app/pages/new_event/new_event_4_page.dart';
 import 'package:party_mobile/app/services/google_places_service.dart';
 import 'package:party_mobile/app/services/navigation_service.dart';
 import 'package:party_mobile/app/shared/constants/app_colors.dart';
+import 'package:party_mobile/app/shared/constants/route_names.dart';
+import 'package:party_mobile/app/shared/utils/google_place_details_formatter.dart';
 import 'package:party_mobile/app/view_models/new_event_vm.dart';
 
 // Page Arguments
@@ -24,6 +27,7 @@ class NewEvent3Page extends StatefulWidget {
 }
 
 class _NewEvent3PageState extends State<NewEvent3Page> {
+  final _formKey = GlobalKey<FormState>();
   NavigationService _navigationService;
   NewEventVM _newEvent;
   String _googleSessionToken;
@@ -75,7 +79,7 @@ class _NewEvent3PageState extends State<NewEvent3Page> {
    * So we set sessionToken = null and if user search for a place again, a new
    * sessionToken will be generated
    */
-  void _getGooglePlaceDetails(dynamic listItem) async {
+  Future<void> _getGooglePlaceDetails(dynamic listItem) async {
     String placeId = listItem['place_id'];
     if (placeId.isNotEmpty) {
       var result = await GooglePlacesService.getPlaceDetails(
@@ -83,9 +87,19 @@ class _NewEvent3PageState extends State<NewEvent3Page> {
       setState(() {
         _selectedPlace = result;
       });
-      print(_selectedPlace);
+      _setNewEventObject();
     }
     _googleSessionToken = null;
+  }
+
+  void _setNewEventObject() {
+    Map formattedAddress =
+        GooglePlaceDetailsFormatter.formatAddress(_selectedPlace);
+    Map formattedLocalization =
+        GooglePlaceDetailsFormatter.formatLocalization(_selectedPlace);
+
+    _newEvent.addressAttributes = formattedAddress;
+    _newEvent.localizationAttributes = formattedLocalization;
   }
 
   void _onTapInputTextField() {
@@ -98,15 +112,25 @@ class _NewEvent3PageState extends State<NewEvent3Page> {
     _inputTextController.text = '';
     setState(() {
       _placesSearchResult = [];
+      _selectedPlace = null;
     });
   }
 
-  void _onTapListTile(dynamic listItem) {
+  void _onTapListTile(dynamic listItem) async {
     _inputTextController.text = listItem['description'];
     setState(() {
       _placesSearchResult = [];
     });
-    _getGooglePlaceDetails(listItem);
+    await _getGooglePlaceDetails(listItem);
+    _formKey.currentState.validate();
+  }
+
+  dynamic addressInputTextValidator(dynamic value) {
+    if (value.isEmpty) return 'O endereço é obrigatório';
+    if (_selectedPlace == null) return 'Selecione um endereço para prosseguir';
+    if (_newEvent.addressAttributes['number'] == null)
+      return 'Seu endereço precisa de um número';
+    return null;
   }
 
   // Widgets
@@ -115,7 +139,14 @@ class _NewEvent3PageState extends State<NewEvent3Page> {
     final _size = MediaQuery.of(context).size;
 
     return RawMaterialButton(
-      onPressed: () {},
+      onPressed: () {
+        if (_formKey.currentState.validate()) {
+          _navigationService.pushNamed(
+            RouteNames.newEvent4,
+            arguments: NewEvent4PageArguments(newEvent: _newEvent),
+          );
+        }
+      },
       child: Container(
         width: _size.width,
         margin: EdgeInsets.symmetric(vertical: _size.height * .007),
@@ -187,39 +218,43 @@ class _NewEvent3PageState extends State<NewEvent3Page> {
                     ),
                   ),
                   SizedBox(height: size.height * .05),
-                  TextFormField(
-                    controller: _inputTextController,
-                    onTap: () {
-                      _onTapInputTextField();
-                    },
-                    onChanged: (value) {
-                      if (value.isNotEmpty) {
-                        _getGooglePlaceResults(value);
-                      }
-                    },
-                    autocorrect: false,
-                    decoration: InputDecoration(
-                      labelText: 'Endereço',
-                      labelStyle: GoogleFonts.inter(
-                        color: AppColors.darkPurple,
-                        fontWeight: FontWeight.bold,
-                        fontSize: size.height * .017,
+                  Form(
+                    key: _formKey,
+                    child: TextFormField(
+                      controller: _inputTextController,
+                      onTap: () {
+                        _onTapInputTextField();
+                      },
+                      onChanged: (value) {
+                        if (value.isNotEmpty) {
+                          _getGooglePlaceResults(value);
+                        }
+                      },
+                      autocorrect: false,
+                      validator: (value) => addressInputTextValidator(value),
+                      decoration: InputDecoration(
+                        labelText: 'Endereço',
+                        labelStyle: GoogleFonts.inter(
+                          color: AppColors.darkPurple,
+                          fontWeight: FontWeight.bold,
+                          fontSize: size.height * .017,
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: AppColors.orange),
+                        ),
+                        suffixIcon: _inputTextController.text.isNotEmpty
+                            ? IconButton(
+                                onPressed: () {
+                                  _onClearTextInputField();
+                                },
+                                icon: Icon(
+                                  Icons.close,
+                                  size: 17,
+                                  color: AppColors.orange,
+                                ),
+                              )
+                            : null,
                       ),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: AppColors.orange),
-                      ),
-                      suffixIcon: _inputTextController.text.isNotEmpty
-                          ? IconButton(
-                              onPressed: () {
-                                _onClearTextInputField();
-                              },
-                              icon: Icon(
-                                Icons.close,
-                                size: 17,
-                                color: AppColors.orange,
-                              ),
-                            )
-                          : null,
                     ),
                   ),
                   SizedBox(height: size.height * .01),
@@ -263,11 +298,72 @@ class _NewEvent3PageState extends State<NewEvent3Page> {
                                 )
                               : SizedBox.shrink(),
                         ),
+                        _selectedPlace != null &&
+                                _placesSearchResult.length == 0 &&
+                                _formKey.currentState.validate()
+                            ? Container(
+                                constraints: BoxConstraints.expand(),
+                                child: SingleChildScrollView(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      SizedBox(height: size.height * .03),
+                                      AutoSizeText(
+                                        'Confirme o endereço',
+                                        style: GoogleFonts.poppins(
+                                          fontSize: size.height * .02,
+                                          color: AppColors.orange,
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                      ),
+                                      SizedBox(height: size.height * .01),
+                                      AutoSizeText(
+                                        _selectedPlace['result']
+                                            ['formatted_address'],
+                                        style: GoogleFonts.poppins(
+                                          fontSize: size.height * .015,
+                                          color: AppColors.darkPurple,
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                      ),
+                                      SizedBox(height: size.height * .03),
+                                      AutoSizeText(
+                                        'Seu endereço tem algum complemento?',
+                                        style: GoogleFonts.poppins(
+                                          fontSize: size.height * .018,
+                                          color: AppColors.orange,
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                      ),
+                                      TextFormField(
+                                        onChanged: (value) {
+                                          _newEvent.addressAttributes[
+                                              'complement'] = value;
+                                        },
+                                        decoration: InputDecoration(
+                                          labelText: 'Apartamento, bloco...',
+                                          labelStyle: GoogleFonts.inter(
+                                            color: AppColors.darkPurple,
+                                            fontSize: size.height * .015,
+                                          ),
+                                          focusedBorder: UnderlineInputBorder(
+                                            borderSide: BorderSide(
+                                              color: AppColors.orange,
+                                            ),
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              )
+                            : SizedBox.shrink(),
                       ],
                     ),
                   ),
                   _continueButton(context),
-                  SizedBox(height: size.height * .02),
+                  SizedBox(height: size.height * .01),
                 ],
               ),
             ),
