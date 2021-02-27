1ff34fb5c16e03c4e653ca834793fc3ffeed86d2
Mateus Veloso <mfvelosos@gmail.com>
2021-02-27 15:47:23 -0300

    ios updates for push notifications

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Mateus Veloso on 27/02/21.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:party_mobile/app/locator.dart';
 import 'package:device_preview/device_preview.dart';
 
 import 'app/app.dart';
 
-void main() {
+void main() async {
   setupLocator();
   runApp(
     DevicePreview(
@@ -12,4 +13,19 @@ void main() {
       builder: (context) => App(),
     ),
   );
+
+  //Remove this method to stop OneSignal Debugging
+
+  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+
+  OneSignal.shared.init("b7cce030-0265-4920-8e30-102a7125a584", iOSSettings: {
+    OSiOSSettings.autoPrompt: false,
+    OSiOSSettings.inAppLaunchUrl: false
+  });
+  OneSignal.shared
+      .setInFocusDisplayType(OSNotificationDisplayType.notification);
+
+// The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  await OneSignal.shared
+      .promptUserForPushNotificationPermission(fallbackToSettings: true);
 }
