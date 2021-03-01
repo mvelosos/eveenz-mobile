5086bc93243c626bd192f40fb57b5d66acbeda58
Mateus Veloso <mfvelosos@gmail.com>
2021-03-01 00:30:42 -0300

    wip apple login

--- lib/app/pages/login/widgets/apple_login_button.dart
+++ lib/app/pages/login/widgets/apple_login_button.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:party_mobile/app/controllers/login_controller.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 class AppleLoginButton extends StatelessWidget {
   final LoginController _loginController;
 
   AppleLoginButton(this._loginController);
+
+  _initAppleLogin() async {
+    AuthorizationCredentialAppleID credential;
+
+    try {
+      credential = await SignInWithApple.getAppleIDCredential(
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+      );
+    } catch (e) {
+      print(e);
+    }
+
+    print(credential);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +30,7 @@ class AppleLoginButton extends StatelessWidget {
 
     return RawMaterialButton(
       onPressed: () {
-        print('login with apple');
+        _initAppleLogin();
       },
       splashColor: Colors.transparent,
       child: Image(
