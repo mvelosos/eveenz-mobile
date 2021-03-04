dc7ef87949b3776df47fea1cd2af1757dc4140a6
Mateus Veloso <mfvelosos@gmail.com>
2021-03-04 10:42:19 -0300

    urls

--- lib/app/shared/constants/urls.dart
+++ lib/app/shared/constants/urls.dart
@@ -1,6 +1,6 @@
 class Urls {
   // Use http for development and https for deploys
-  static const String protocol = 'https';
+  static const String protocol = 'http';
 
   // Always v1
   static const String apiVersion = 'v1';
@@ -9,13 +9,13 @@ class Urls {
   // static const String baseUrl = '10.0.2.2:3000';
 
   // Use when running on real device
-  // static const String baseUrl = '192.168.0.12:3000';
+  static const String baseUrl = '192.168.0.12:3000';
 
   // Use when running on iPhone Simulator
   // static const String baseUrl = '127.0.0.1:3000';
 
   // Production URL
-  static const String baseUrl = 'eveenz-production.herokuapp.com';
+  // static const String baseUrl = 'eveenz-production.herokuapp.com';
 
   String get apiUrl {
     return '$protocol://$baseUrl/api/$apiVersion';
