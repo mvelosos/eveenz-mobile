c2c705fb1241912ee21b4e081407c351acb2ad85
Mateus Veloso <mfvelosos@gmail.com>
2021-03-04 23:53:04 -0300

    created date picker and time picker shared component

---
+++ lib/app/shared/widgets/date_picker.dart
@@ -0,0 +1,76 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DatePicker {
+  BuildContext _context;
+  DateTime _selectedDate;
+  Function _setDate;
+
+  DatePicker(context, selectedDate, setDate) {
+    this._context = context;
+    this._selectedDate = selectedDate;
+    this._setDate = setDate;
+    this._selectDate();
+  }
+
+  _selectDate() {
+    if (Platform.isIOS) {
+      return buildCupertinoDatePicker(_context);
+    } else {
+      return buildMaterialDatePicker(_context);
+    }
+  }
+
+  buildMaterialDatePicker(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: _selectedDate,
+      firstDate: DateTime.now(),
+      lastDate: DateTime(DateTime.now().year + 2),
+      builder: (context, child) {
+        return Theme(
+          data: ThemeData.light(),
+          child: child,
+        );
+      },
+    );
+    if (picked != null && picked != _selectedDate) _setDate(picked);
+  }
+
+  buildCupertinoDatePicker(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext builder) {
+        return Container(
+          height: MediaQuery.of(context).copyWith().size.height / 3,
+          color: Colors.white,
+          child: Stack(
+            children: [
+              CupertinoDatePicker(
+                mode: CupertinoDatePickerMode.date,
+                onDateTimeChanged: (picked) {
+                  if (picked != null && picked != _selectedDate)
+                    _setDate(picked);
+                },
+                initialDateTime: _selectedDate,
+                minimumYear: DateTime.now().year,
+                maximumYear: DateTime.now().year + 2,
+              ),
+              Positioned(
+                right: 0,
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Ok'),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/app/shared/widgets/time_picker.dart
@@ -0,0 +1,71 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TimePicker {
+  BuildContext _context;
+  DateTime _selectedTime;
+  Function _setTime;
+
+  TimePicker(context, selectedDate, setTime) {
+    this._context = context;
+    this._selectedTime = selectedDate;
+    this._setTime = setTime;
+    this._selectTime();
+  }
+
+  _selectTime() async {
+    if (Platform.isIOS) {
+      return _buildCupertinoTimePicker(_context);
+    } else {
+      return _buildMaterialTimePicker(_context);
+    }
+  }
+
+  _buildMaterialTimePicker(BuildContext context) async {
+    TimeOfDay _currentTime = TimeOfDay.fromDateTime(_selectedTime);
+    final picked =
+        await showTimePicker(context: context, initialTime: _currentTime);
+    if (picked != null && picked != _currentTime) {
+      DateTime date = DateTime.now();
+      DateTime newTime =
+          DateTime(date.year, date.month, date.day, picked.hour, picked.minute);
+      _setTime(newTime);
+    }
+  }
+
+  _buildCupertinoTimePicker(BuildContext context) async {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext builder) {
+        return Container(
+          height: MediaQuery.of(context).copyWith().size.height / 3,
+          color: Colors.white,
+          child: Stack(
+            children: [
+              CupertinoDatePicker(
+                mode: CupertinoDatePickerMode.time,
+                onDateTimeChanged: (picked) {
+                  if (picked != null && picked != _selectedTime)
+                    _setTime(picked);
+                },
+                use24hFormat: true,
+                initialDateTime: _selectedTime,
+              ),
+              Positioned(
+                right: 0,
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Ok'),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
