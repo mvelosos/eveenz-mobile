543322529a042ebcac8eac51e6d6376a7e87f0ba
Mateus Veloso <mfvelosos@gmail.com>
2021-03-05 23:38:03 -0300

    add sentry DNS to api_keys

--- lib/app/shared/constants/api_keys.dart
+++ lib/app/shared/constants/api_keys.dart
@@ -1,4 +1,6 @@
 class ApiKeys {
   static const String kGoogleApiKey = 'AIzaSyApNjv96BtchkBY9pJSdQAAlKpi17Hr_5o';
   static const String kOneSignalKey = 'b7cce030-0265-4920-8e30-102a7125a584';
+  static const String kSentryDns =
+      'https://6368662f9daf43a49248d6ad8c691702@o312062.ingest.sentry.io/5664485';
 }
