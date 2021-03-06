92c98791303372966f9887aecabef6308f147160
Mateus Veloso <mfvelosos@gmail.com>
2021-03-05 23:37:54 -0300

    updated urls to automatically check releaseMode

--- lib/app/shared/constants/urls.dart
+++ lib/app/shared/constants/urls.dart
@@ -1,23 +1,25 @@
-class Urls {
-  // Use http for development and https for deploys
-  static const String protocol = 'https';
+import 'package:flutter/foundation.dart';
 
-  // Always v1
-  static const String apiVersion = 'v1';
+class Urls {
+  static String prodProtocol = 'https';
+  static String prodBaseUrl = 'eveenz-production.herokuapp.com';
+  static String prodApiVersion = 'v1';
+
+  static String protocol = 'http';
+  static String apiVersion = 'v1';
+  static String baseUrl = '192.168.0.12:3000';
 
   //Use Android Emulator ip when running on Android Emulators
-  // static const String baseUrl = '10.0.2.2:3000';
-
-  // Use when running on real device
-  // static const String baseUrl = '192.168.0.12:3000';
+  // static String baseUrl = '10.0.2.2:3000';
 
   // Use when running on iPhone Simulator
-  // static const String baseUrl = '127.0.0.1:3000';
-
-  // Production URL
-  static const String baseUrl = 'eveenz-production.herokuapp.com';
+  // static String baseUrl = '127.0.0.1:3000';
 
   String get apiUrl {
+    if (kReleaseMode) {
+      return '$prodProtocol://$prodBaseUrl/api/$prodApiVersion';
+    }
+
     return '$protocol://$baseUrl/api/$apiVersion';
   }
 }
