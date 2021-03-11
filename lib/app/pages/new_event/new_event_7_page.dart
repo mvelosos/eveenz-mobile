5f31226dd14825a0fcc8a4dd921f858db9631543
Mateus Veloso <mfvelosos@gmail.com>
2021-03-11 17:21:56 -0300

    wip new_event_7_page

--- lib/app/pages/new_event/new_event_7_page.dart
+++ lib/app/pages/new_event/new_event_7_page.dart
@@ -1,4 +1,6 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:party_mobile/app/services/navigation_service.dart';
 import 'package:party_mobile/app/shared/constants/app_colors.dart';
 import 'package:party_mobile/app/view_models/new_event_vm.dart';
@@ -36,6 +38,8 @@ class _NewEvent7PageState extends State<NewEvent7Page> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return LayoutBuilder(
       builder: (context, constraints) {
         return Scaffold(
@@ -45,7 +49,63 @@ class _NewEvent7PageState extends State<NewEvent7Page> {
             shadowColor: Colors.transparent,
             brightness: Brightness.light,
           ),
-          body: Center(child: Text('next screen')),
+          body: GestureDetector(
+            onTap: () {
+              FocusScope.of(context).unfocus();
+            },
+            child: Container(
+              constraints: BoxConstraints.expand(),
+              padding: EdgeInsets.only(
+                left: size.width * .08,
+                right: size.width * .08,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: size.height * .02),
+                  Text(
+                    'Sobre o evento',
+                    style: GoogleFonts.inter(
+                      fontSize: size.height * .033,
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.darkPurple,
+                    ),
+                  ),
+                  SizedBox(height: size.height * .005),
+                  AutoSizeText(
+                    'Você pode escolher até 3 categorias que seu evento se encaixa.',
+                    style: GoogleFonts.poppins(
+                      fontSize: size.height * .016,
+                      color: AppColors.darkPurple,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  Chip(
+                    label: Text(
+                      'Aaron Burr',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    backgroundColor: Colors.pink,
+                    deleteIcon: Icon(Icons.close),
+                    onDeleted: () {
+                      print('deleted');
+                    },
+                  ),
+                  Chip(
+                    label: Text(
+                      'Joãozin',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    backgroundColor: Colors.blue,
+                    deleteIcon: Icon(Icons.close),
+                    onDeleted: () {
+                      print('deleted');
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
         );
       },
     );
