f740a2467ac84279ac36212a3d7e0933e9d4001d
Mateus Veloso <mfvelosos@gmail.com>
2021-03-14 12:28:46 -0300

    wip categories_controller

---
+++ lib/app/controllers/categories_controller.dart
@@ -0,0 +1,28 @@
+import 'package:dartz/dartz.dart';
+import 'package:party_mobile/app/models/category_model.dart';
+import 'package:party_mobile/app/repositories/categories_repository.dart';
+import 'package:party_mobile/app/shared/errors/errors.dart';
+
+class CategoriesController {
+  CategoriesRepository _categoriesRepository;
+
+  CategoriesController() {
+    _categoriesRepository = CategoriesRepository();
+  }
+
+  Future<Either<Failure, List<CategoryModel>>> getCategories() async {
+    List<CategoryModel> categoriesList = [];
+    var result = await _categoriesRepository.getCategories();
+    if (result.isRight()) {
+      dynamic resultList = result.getOrElse(null);
+      if (resultList['categories'] != null || resultList['categories'] != []) {
+        resultList['categories'].forEach(
+          (category) => categoriesList.add(
+            CategoryModel.fromJson(category),
+          ),
+        );
+      }
+    }
+    return Right(categoriesList);
+  }
+}
