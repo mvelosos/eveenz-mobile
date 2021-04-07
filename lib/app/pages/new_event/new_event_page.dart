53f5ea6496afb965416e087e9a18e19b0342f980
Mateus Veloso <mfvelosos@gmail.com>
2021-04-07 09:54:46 -0300

    created new_event_page

---
+++ lib/app/pages/new_event/new_event_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:party_mobile/app/shared/constants/app_colors.dart';
+
+class NewEventPage extends StatefulWidget {
+  @override
+  _NewEventPageState createState() => _NewEventPageState();
+}
+
+class _NewEventPageState extends State<NewEventPage> {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraint) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            iconTheme: IconThemeData(color: AppColors.orange),
+            shadowColor: Colors.transparent,
+            brightness: Brightness.light,
+          ),
+          body: Center(
+            child: Text('new event'),
+          ),
+        );
+      },
+    );
+  }
+}
