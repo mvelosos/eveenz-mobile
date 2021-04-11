ce6e778311d2f94487369231af62fc98b5c2e9c1
Mateus Veloso <mfvelosos@gmail.com>
2021-04-11 00:39:46 -0300

    image_upload_model

---
+++ lib/app/models/image_upload_model.dart
@@ -0,0 +1,19 @@
+import 'package:image_picker/image_picker.dart';
+
+class ImageUploadModel {
+  bool isUploaded;
+  bool uploading;
+  PickedFile imageFile;
+  String imageUrl;
+
+  ImageUploadModel({
+    this.isUploaded,
+    this.uploading,
+    this.imageFile,
+    this.imageUrl,
+  });
+
+  get imagePath {
+    return imageFile.path;
+  }
+}
