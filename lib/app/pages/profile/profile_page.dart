532decc99de7cbec2701dd226f1485f4d71f5298
Mateus Veloso <mfvelosos@gmail.com>
2021-04-14 20:51:53 -0300

    wip

--- lib/app/pages/profile/profile_page.dart
+++ lib/app/pages/profile/profile_page.dart
@@ -1,9 +1,13 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:party_mobile/app/controllers/me_controller.dart';
 import 'package:party_mobile/app/locator.dart';
 import 'package:party_mobile/app/pages/follows/follows_page.dart';
 import 'package:party_mobile/app/services/navigation_service.dart';
+import 'package:party_mobile/app/shared/constants/app_colors.dart';
 import 'package:party_mobile/app/shared/constants/route_names.dart';
 import 'package:party_mobile/app/stores/me_store.dart';
 
@@ -19,16 +23,17 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
+    Size _size = MediaQuery.of(context).size;
     _navigationService = NavigationService.currentNavigator(context);
 
     return Scaffold(
       appBar: AppBar(
-        title: Obx(
-          () => Text(
-            _meStore.username.value,
-            style: TextStyle(color: Colors.blue),
-          ),
-        ),
+        // title: Obx(
+        //   () => Text(
+        //     _meStore.username.value,
+        //     style: TextStyle(color: Colors.blue),
+        //   ),
+        // ),
         centerTitle: true,
         backgroundColor: Colors.transparent,
         shadowColor: Colors.transparent,
@@ -36,7 +41,10 @@ class _ProfilePageState extends State<ProfilePage> {
         automaticallyImplyLeading: false,
         actions: [
           IconButton(
-            icon: Icon(Icons.view_headline),
+            icon: FaIcon(
+              FontAwesomeIcons.cog,
+              size: 20,
+            ),
             color: Colors.grey,
             tooltip: 'Show Snackbar',
             onPressed: () {
@@ -57,69 +65,17 @@ class _ProfilePageState extends State<ProfilePage> {
               physics: AlwaysScrollableScrollPhysics(),
               scrollDirection: Axis.vertical,
               child: Container(
-                padding: EdgeInsets.only(left: 10, right: 10),
                 height: constraints.maxHeight,
                 width: constraints.maxWidth,
+                padding: EdgeInsets.only(
+                  left: _size.width * .06,
+                  right: _size.width * .06,
+                ),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        Column(
-                          children: [
-                            Obx(
-                              () => Text(
-                                _meStore.events.value.toString(),
-                                style: TextStyle(fontSize: 19),
-                              ),
-                            ),
-                            Text('Festas'),
-                          ],
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            _navigationService.pushNamed(
-                              RouteNames.accountFollows,
-                              arguments: FollowsPageArguments(
-                                username: _meStore.username.value,
-                                initialIndex: 0,
-                              ),
-                            );
-                          },
-                          child: Column(
-                            children: [
-                              Obx(
-                                () => Text(
-                                  _meStore.followers.value.toString(),
-                                  style: TextStyle(fontSize: 19),
-                                ),
-                              ),
-                              Text('Seguidores'),
-                            ],
-                          ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            _navigationService.pushNamed(
-                              RouteNames.accountFollows,
-                              arguments: FollowsPageArguments(
-                                username: _meStore.username.value,
-                                initialIndex: 1,
-                              ),
-                            );
-                          },
-                          child: Column(
-                            children: [
-                              Obx(
-                                () => Text(
-                                  _meStore.following.value.toString(),
-                                  style: TextStyle(fontSize: 19),
-                                ),
-                              ),
-                              Text('Seguindo'),
-                            ],
-                          ),
-                        ),
                         ClipRRect(
                           borderRadius: BorderRadius.circular(50),
                           child: Obx(
@@ -127,8 +83,8 @@ class _ProfilePageState extends State<ProfilePage> {
                               return _meStore.avatarUrl.value != ''
                                   ? Image.network(
                                       _meStore.avatarUrl.value,
-                                      height: 85,
-                                      width: 85,
+                                      height: 95,
+                                      width: 95,
                                     )
                                   : CircleAvatar(
                                       radius: 45,
@@ -137,10 +93,122 @@ class _ProfilePageState extends State<ProfilePage> {
                             },
                           ),
                         ),
+                        Expanded(
+                          child: Container(
+                            margin: EdgeInsets.only(left: 20),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Obx(
+                                  () => AutoSizeText(_meStore.name.toString(),
+                                      minFontSize: 16,
+                                      // maxFontSize: 22,
+                                      // overflow: TextOverflow.ellipsis,
+                                      style: GoogleFonts.inter(
+                                        fontWeight: FontWeight.bold,
+                                        color: AppColors.darkPurple,
+                                        fontSize: 22,
+                                      )),
+                                ),
+                                Obx(
+                                  () => AutoSizeText(
+                                      '@${_meStore.username.toString()}',
+                                      minFontSize: 15,
+                                      style: GoogleFonts.inter(
+                                        color: AppColors.gray1,
+                                        fontSize: 5,
+                                      )),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
                       ],
                     ),
                   ],
                 ),
+                // child: Column(
+                //   children: [
+                //     Row(
+                //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                //       children: [
+                //         Column(
+                //           children: [
+                //             Obx(
+                //               () => Text(
+                //                 _meStore.events.value.toString(),
+                //                 style: TextStyle(fontSize: 19),
+                //               ),
+                //             ),
+                //             Text('Festas'),
+                //           ],
+                //         ),
+                //         GestureDetector(
+                //           onTap: () {
+                //             _navigationService.pushNamed(
+                //               RouteNames.accountFollows,
+                //               arguments: FollowsPageArguments(
+                //                 username: _meStore.username.value,
+                //                 initialIndex: 0,
+                //               ),
+                //             );
+                //           },
+                //           child: Column(
+                //             children: [
+                //               Obx(
+                //                 () => Text(
+                //                   _meStore.followers.value.toString(),
+                //                   style: TextStyle(fontSize: 19),
+                //                 ),
+                //               ),
+                //               Text('Seguidores'),
+                //             ],
+                //           ),
+                //         ),
+                //         GestureDetector(
+                //           onTap: () {
+                //             _navigationService.pushNamed(
+                //               RouteNames.accountFollows,
+                //               arguments: FollowsPageArguments(
+                //                 username: _meStore.username.value,
+                //                 initialIndex: 1,
+                //               ),
+                //             );
+                //           },
+                //           child: Column(
+                //             children: [
+                //               Obx(
+                //                 () => Text(
+                //                   _meStore.following.value.toString(),
+                //                   style: TextStyle(fontSize: 19),
+                //                 ),
+                //               ),
+                //               Text('Seguindo'),
+                //             ],
+                //           ),
+                //         ),
+                //         ClipRRect(
+                //           borderRadius: BorderRadius.circular(50),
+                //           child: Obx(
+                //             () {
+                //               return _meStore.avatarUrl.value != ''
+                //                   ? Image.network(
+                //                       _meStore.avatarUrl.value,
+                //                       height: 85,
+                //                       width: 85,
+                //                     )
+                //                   : CircleAvatar(
+                //                       radius: 45,
+                //                       backgroundColor: Color(0xffd3d5db),
+                //                     );
+                //             },
+                //           ),
+                //         ),
+                //       ],
+                //     ),
+                //   ],
+                // ),
               ),
             ),
           );
