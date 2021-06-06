2566793b834c6918f81521cb1a6593f529657ca3
Mateus Veloso <mfvelosos@gmail.com>
2021-06-06 20:23:54 -0300

    update onesginal init call

--- lib/app/one_signal.dart
+++ lib/app/one_signal.dart
@@ -2,11 +2,5 @@ import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:party_mobile/app/shared/constants/api_keys.dart';
 
 void setupOneSignal() async {
-  OneSignal.shared.init(ApiKeys.kOneSignalKey, iOSSettings: {
-    OSiOSSettings.autoPrompt: false,
-    OSiOSSettings.inAppLaunchUrl: false
-  });
-
-  OneSignal.shared
-      .setInFocusDisplayType(OSNotificationDisplayType.notification);
+  OneSignal.shared.setAppId(ApiKeys.kOneSignalKey);
 }
