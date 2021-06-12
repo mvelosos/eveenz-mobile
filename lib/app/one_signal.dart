a0f2b9da3ef46a53f1934c8bc6d769b48382d482
Mateus Veloso <mfvelosos@gmail.com>
2021-06-12 01:05:37 -0300

    set onesignal

--- lib/app/one_signal.dart
+++ lib/app/one_signal.dart
@@ -3,4 +3,17 @@ import 'package:party_mobile/app/shared/constants/api_keys.dart';
 
 void setupOneSignal() async {
   OneSignal.shared.setAppId(ApiKeys.kOneSignalKey);
+
+  // OneSignal.shared.setAppId("YOUR_ONESIGNAL_APP_ID");
+
+  // OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+  //   print("Accepted permission: $accepted");
+  // });
+  // OneSignal Initialization
+// var settings = {
+//   OSiOSSettings.autoPrompt: false,
+//   OSiOSSettings.promptBeforeOpeningPushUrl: true
+// };
+// OneSignal.shared
+//         .init(ApiKeys.kOneSignalKey, iOSSettings: settings);
 }
